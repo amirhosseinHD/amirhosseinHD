12eabb2eb5693720ec337af92552b5bfdf4f70ec
amirhosseinHD <83590626+amirhosseinHD@users.noreply.github.com>
2023-06-27 21:00:23 +0330

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -1,9 +1,11 @@
+#import the library's we need
 import time , pyautogui
+#get the text and number of text
 inp = input("enter your text for spam : ")
 inp2 = input("how much? ")
-
 inp2 = int(inp2)
 inp2 = inp2 + 1
+#*if you want too change sleep time change the number*
 time.sleep(5)
 for i in range(1,inp2):
     pyautogui.typewrite(inp)
